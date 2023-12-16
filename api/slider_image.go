5cc86b66cac15dac1eaa06d00f6912f2227e0a68
mosleh <moslehazizi@gmail.com>
2023-12-16 18:35:28 +0330

    Complete create slider_image api

--- api/slider_image.go
+++ api/slider_image.go
@@ -4,18 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-
 )
 
 type postSliderImageRequest struct {
 	ImagePath string `json:"image_path" binding:"required"`
 }
 
-func (server *Server) postSliderImage (c *gin.Context) {
+func (server *Server) postSliderImage(c *gin.Context) {
 	var req postSliderImageRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
+	slider_image, err := server.store.CreateSliderImage(c, req.ImagePath)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, slider_image)
 }
