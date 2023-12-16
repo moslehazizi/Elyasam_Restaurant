3a90d3d7f3d5833763ac732bd4c027d6db8c4cb9
mosleh <moslehazizi@gmail.com>
2023-12-16 17:53:37 +0330

    Add discount_ofer and slider_image tables

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -11,14 +11,20 @@ import (
 type Querier interface {
 	CreateCategory(ctx context.Context, arg CreateCategoryParams) (Category, error)
 	CreateComment(ctx context.Context, arg CreateCommentParams) (Comment, error)
+	CreateDiscountOffer(ctx context.Context, serviceID int64) (DiscountOffer, error)
 	CreateService(ctx context.Context, arg CreateServiceParams) (Service, error)
+	CreateSliderImage(ctx context.Context, imagePath string) (SliderImage, error)
 	GetCategory(ctx context.Context, id int64) (Category, error)
 	GetComment(ctx context.Context, id int64) (Comment, error)
+	GetDiscountOffer(ctx context.Context, id int64) (DiscountOffer, error)
 	GetService(ctx context.Context, id int64) (Service, error)
+	GetSliderImage(ctx context.Context, id int64) (SliderImage, error)
 	ListCategories(ctx context.Context, arg ListCategoriesParams) ([]Category, error)
 	ListComments(ctx context.Context, arg ListCommentsParams) ([]Comment, error)
+	ListDiscountOffers(ctx context.Context, arg ListDiscountOffersParams) ([]DiscountOffer, error)
 	ListServices(ctx context.Context, arg ListServicesParams) ([]Service, error)
 	ListServicesById(ctx context.Context, arg ListServicesByIdParams) ([]Service, error)
+	ListSliderImages(ctx context.Context, arg ListSliderImagesParams) ([]SliderImage, error)
 	UpdateService(ctx context.Context, arg UpdateServiceParams) (Service, error)
 }
 
